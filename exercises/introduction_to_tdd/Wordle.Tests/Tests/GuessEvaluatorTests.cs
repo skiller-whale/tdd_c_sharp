e0590ee58c6675f199a73d1dc768174aef1f15e9
Amyas Merivale <merivale@gmail.com>
2025-09-17 18:12:58 +0200

    Update exercises

---
+++ exercises/introduction_to_tdd/Wordle.Tests/Tests/GuessEvaluatorTests.cs
@@ -0,0 +1,31 @@
+using Xunit;
+using Wordle.Core;
+
+namespace Wordle.Tests;
+
+public class GuessEvaluatorTests
+{
+    [Fact(Skip = "Not implemented yet")]
+    public void EvaluateGuess_ReturnsDashes_ForCompletelyIncorrectGuess()
+    {
+        // TODO: Implement test
+    }
+
+    [Fact(Skip = "Not implemented yet")]
+    public void EvaluateGuess_ReturnsGGGGG_ForCompletelyCorrectGuess()
+    {
+        // TODO: Implement test
+    }
+
+    [Fact(Skip = "Not implemented yet")]
+    public void EvaluateGuess_IncludesG_ForOneCorrectLetterInCorrectPosition()
+    {
+        // TODO: Implement test
+    }
+
+    [Fact(Skip = "Not implemented yet")]
+    public void EvaluateGuess_IncludesY_ForOneCorrectLetterInWrongPosition()
+    {
+        // TODO: Implement test
+    }
+}
---
+++ exercises/introduction_to_tdd/Wordle/Core/GuessEvaluator.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Wordle.Core;
+
+public class GuessEvaluator
+{
+    // Your implementation goes here
+    public static string EvaluateGuess(string secretWord, string guess)
+    {
+        return "-----";
+    }
+}
